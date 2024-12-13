2daad9d22740b1596648402b19f76eadcaecc59d
kangwonsik07 <alucard815@naver.com>
2024-12-13 11:46:50 +0900

    first commit

---
+++ board-frontend/src/App.js
@@ -0,0 +1,12 @@
+import Home from './pages/home'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+   return (
+      <Routes>
+         <Route path="/" element={<Home />} />
+      </Routes>
+   )
+}
+
+export default App
